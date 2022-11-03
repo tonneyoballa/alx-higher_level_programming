94d033dad9bbdd7a0244ecbad6c3b7223a758d4d
tonneyoballa <tonneyoballah@gmail.com>
2022-11-04 02:04:21 +0300

    task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records of a table
+-- Query to list all records of the table second_table that have name value
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

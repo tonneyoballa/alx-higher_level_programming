5a7bc2b73721fbc9b5702766e220ecbd443825df
tonneyoballa <tonneyoballah@gmail.com>
2022-11-30 02:03:58 +0300

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (let col = 0; col < this.height; col += 1) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+}
+  
+module.exports = Rectangle;

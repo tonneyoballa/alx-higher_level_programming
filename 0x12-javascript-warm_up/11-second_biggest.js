20479c491294763ace2f9955db44077649ced2fe
tonneyoballa <tonneyoballah@gmail.com>
2022-11-29 04:33:22 +0300

    fixx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-console.log(0);
+
+const argv = process.argv;
+
+if (argv.length <= 3) {
+  console.log(0);
 } else {
-const args = process.argv.map(Number)
-.slice(2, process.argv.length)
-.sort((a, b) => a - b);
-console.log(args[args.length - 2]);
+  const maxsecond = argv.sort(function (a, b) { return b - a; })[3];
+  console.log(maxsecond);
 }

07ab8750d10a914f0e0e5f8a592ef33dc608a8dc
tonneyoballa <tonneyoballah@gmail.com>
2022-11-29 04:25:28 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 function factorial (a) {
-    if ((Number.isNaN(a)) || (a === 1)) {
-      return 1;
-    }
-    return factorial(a - 1) * a;
+  if (a === 1 || isNaN(a)) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+const a = parseInt(process.argv[2]);
+console.log(factorial(a));

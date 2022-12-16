d0a0c485ba7aa3f15cd247cd2f36866bf1264b2c
tonneyoballa <tonneyoballah@gmail.com>
2022-12-16 20:28:11 +0300

    Fix 4 & 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-Python script that sends a request to the URL and
-displays the value of a variable in the response header
+"""  displays the value of the X-Request-Id variable
+    found in the header of the response
 """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except:
-        pass
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))

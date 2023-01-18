1d656f8655aa91ad573bc60a8c9c7a09ebb30f6a
tonneyoballa <tonneyoballah@gmail.com>
2023-01-18 16:21:59 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
+
 const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body.title);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
   }
 });
